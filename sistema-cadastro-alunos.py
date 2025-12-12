6e5673a05885ae1a02aeb497d771e60e53885e57
luizinn0213 <luizfelipeoliveiraviana@gmail.com>
2025-12-12 00:39:41 -0300

    Commit funções pesquisar_aluno, editar_aluno

--- sistema-cadastro-alunos.py
+++ sistema-cadastro-alunos.py
@@ -48,6 +48,80 @@ def inserir_aluno(df):
     print("\nAluno cadastrado com sucesso! Matrícula:", matricula)
     return df
 
+#  Pesquisar aluno
+def pesquisar_aluno(df):
+    print("\n=== PESQUISAR ALUNO ===")
+    print("1 - Por matrícula")
+    print("2 - Por nome")
+    opc = input("Escolha a opção: ")
+
+    if opc == "1":
+        mat = input("Digite a matrícula: ")
+        if not mat.isdigit():
+            print("Matrícula inválida!")
+            return None
+
+        resultado = df[df["matricula"] == int(mat)]
+
+    elif opc == "2":
+        nome = input("Digite o nome: ")
+        resultado = df[df["nome"].str.contains(nome, case=False, na=False)]
+    else:
+        print("Opção inválida!")
+        return None
+
+    if resultado.empty:
+        print("\nAluno NÃO encontrado!")
+        return None
+
+    print("\n--- ALUNO ENCONTRADO ---")
+    print(resultado.to_string(index=False))
+
+    return resultado.iloc[0]
+
+#  Editar aluno
+def editar_aluno(df, aluno):
+    print("\n=== EDITAR ALUNO ===")
+    print("Quais dados deseja editar?")
+    print("1 - Nome")
+    print("2 - Rua")
+    print("3 - Número")
+    print("4 - Bairro")
+    print("5 - Cidade")
+    print("6 - UF")
+    print("7 - Telefone")
+    print("8 - Email")
+    print("9 - Cancelar")
+
+    opc = input("Escolha a opção: ")
+
+    campos = {
+        "1": "nome",
+        "2": "rua",
+        "3": "numero",
+        "4": "bairro",
+        "5": "cidade",
+        "6": "uf",
+        "7": "telefone",
+        "8": "email"
+    }
+
+    if opc == "9":
+        print("Edição cancelada.")
+        return df
+
+    if opc not in campos:
+        print("Opção inválida!")
+        return df
+
+    campo = campos[opc]
+    novo_valor = input(f"Digite o novo valor para {campo}: ")
+
+    df.loc[df["matricula"] == aluno["matricula"], campo] = novo_valor
+    salvar_dados(df)
+
+    print("\nDados atualizados com sucesso!")
+    return df
 
 #  Main
 def menu():
