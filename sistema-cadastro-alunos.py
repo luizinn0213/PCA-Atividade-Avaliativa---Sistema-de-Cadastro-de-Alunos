64b85f8cfa1e8f5767c7a0cd1534a6f0a78a12ef
luizinn0213 <luizfelipeoliveiraviana@gmail.com>
2025-12-12 00:33:41 -0300

    Commit Main

---
+++ sistema-cadastro-alunos.py
@@ -0,0 +1,39 @@
+#  Main
+def menu():
+    df = carregar_dados()
+
+    while True:
+        print("\n==== SISTEMA DE ALUNOS ====")
+        print("1 - Inserir")
+        print("2 - Pesquisar")
+        print("3 - Sair")
+
+        opc = input("Escolha a opção: ")
+
+        if opc == "1":
+            df = inserir_aluno(df)
+
+        elif opc == "2":
+            aluno = pesquisar_aluno(df)
+            if aluno is not None:
+                print("\nDeseja:")
+                print("1 - Editar")
+                print("2 - Remover")
+                print("3 - Voltar")
+                acao = input("Escolha: ")
+
+                if acao == "1":
+                    df = editar_aluno(df, aluno)
+                elif acao == "2":
+                    df = remover_aluno(df, aluno)
+                else:
+                    print("Voltando ao menu...")
+
+        elif opc == "3":
+            print("Saindo do sistema...")
+            break
+        else:
+            print("Opção inválida!")
+
+# Executa o menu
+menu()
